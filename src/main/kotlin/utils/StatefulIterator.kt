be59e85ed20bde3e693b9a148dc828207739d821
peawyoyoyin <peawyoyoyin@gmail.com>
2022-12-11 01:16:07 +0800

    use protected instead of internal

--- src/main/kotlin/utils/StatefulIterator.kt
+++ src/main/kotlin/utils/StatefulIterator.kt
@@ -2,7 +2,7 @@ package utils
 
 // simple wrapper for stateful iterator, useful for suspendable executions
 abstract class StatefulIterator<T>: Iterator<T> {
-    internal abstract val underlyingIterator: Iterator<T>
+    protected abstract val underlyingIterator: Iterator<T>
 
     override fun hasNext(): Boolean = underlyingIterator.hasNext()
     override fun next(): T = underlyingIterator.next()
