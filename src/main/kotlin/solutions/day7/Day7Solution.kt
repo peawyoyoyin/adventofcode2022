23d034724301d7f91b231293ea0483de56aed7c9
peawyoyoyin <peawyoyoyin@gmail.com>
2022-12-11 01:09:32 +0800

    refactor day 7

--- src/main/kotlin/solutions/day7/Day7Solution.kt
+++ src/main/kotlin/solutions/day7/Day7Solution.kt
@@ -4,24 +4,6 @@ import utils.InputReader
 import utils.measureAndLogTime
 import java.util.LinkedList
 
-interface Node {
-    val size: Long
-}
-
-data class File(val name: String, override val size: Long): Node
-data class Directory(val name: String, val contents: MutableMap<String, Node>): Node {
-    override val size by lazy { contents.values.sumOf { it.size } }
-
-    companion object {
-        fun listAllSubDirectories(directory: Directory): List<Directory> {
-            val subDirectories = directory.contents.values
-                .filterIsInstance<Directory>()
-                .flatMap { listAllSubDirectories(it) }
-            return listOf(directory) +  subDirectories
-        }
-    }
-}
-
 val commandRegex = Regex("\\$ .*")
 
 val cdRegex = Regex("\\$ cd (.*)")
---
+++ src/main/kotlin/solutions/day7/Node.kt
@@ -0,0 +1,19 @@
+package solutions.day7
+
+interface Node {
+    val size: Long
+}
+
+data class File(val name: String, override val size: Long): Node
+data class Directory(val name: String, val contents: MutableMap<String, Node>): Node {
+    override val size by lazy { contents.values.sumOf { it.size } }
+
+    companion object {
+        fun listAllSubDirectories(directory: Directory): List<Directory> {
+            val subDirectories = directory.contents.values
+                .filterIsInstance<Directory>()
+                .flatMap { listAllSubDirectories(it) }
+            return listOf(directory) +  subDirectories
+        }
+    }
+}
